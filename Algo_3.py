cb896d96432bd5dffbd53365fa38ea9bddfbebed
hassam <hassam.ansar22007@gmail.com>
2026-09-15 21:57:17 +0500

    Create and Update Algo_3.py

---
+++ Algo_3.py
@@ -0,0 +1,10 @@
+
+# Algorithm_3:
+
+list_1 = [10, 20, 30, 40, 50]
+list_2 = [30, 40, 50, 60, 70]
+
+for i in list_1:
+    for j in list_2:
+        if(i == j):
+            print("Common element:", i)
